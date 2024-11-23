93cf27a524ea49eb567b0d686ea2da67537339eb
Chethaka Sathmal <csathmal24@gmail.com>
2024-11-23 16:44:33 +0530

    added nativewind(tailwind) to project

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 
 const Welcome = () => {
-  return <Text>Welcome</Text>;
+  return <Text className="font-bold bg-red-200">Welcome</Text>;
 };
 
 export default Welcome;
