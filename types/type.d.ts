475122147427656becec58ff73b0baa26c61fe16
Chethaka Sathmal <csathmal24@gmail.com>
2024-11-24 18:00:09 +0530

    added custome types

---
+++ types/type.d.ts
@@ -0,0 +1,26 @@
+import { TouchableOpacityProps } from "react-native";
+
+declare interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+  textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+  className?: string;
+}
+
+interface FontSizeTypes {
+  sm: number;
+  base: number;
+  xl: number;
+  "2xl": number;
+  "3xl": number;
+  "4xl": number;
+  "5xl": number;
+}
+
+interface DimensionTypes {
+  ww: number;
+  wh: number;
+  diameter: number;
+}
