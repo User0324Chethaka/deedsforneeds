8e633150bec6f7d9b31df563d343e430a13750ab
Chethaka Sathmal <csathmal24@gmail.com>
2024-11-24 18:00:53 +0530

    centralized all the constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,22 +1,27 @@
 import { Dimensions } from "react-native";
-import { scaleByWidth } from "@/utils/dimensions";
+import welcome from "@/assets/images/welcome.webp";
+
+import { FontSizeTypes, DimensionTypes } from "@/types/type";
 
 const { width, height, fontScale } = Dimensions.get("window");
+const baseFontSize: number = width * 0.035 * fontScale;
 
-export const dimensions = {
+export const dimensions: DimensionTypes = {
   ww: width,
   wh: height,
   diameter: Math.sqrt(width ** 2 + height ** 2),
 };
 
-const baseFontSize: number = scaleByWidth(0.02) * fontScale;
+export const fontSizes: FontSizeTypes = {
+  sm: baseFontSize * 0.8,
+  base: baseFontSize,
+  xl: baseFontSize * 1.25,
+  "2xl": baseFontSize * 1.563,
+  "3xl": baseFontSize * 1.953,
+  "4xl": baseFontSize * 2.441,
+  "5xl": baseFontSize * 3.052,
+};
 
-export const fontSizes = {
-  sm: `${baseFontSize * 0.8}px`,
-  base: `${baseFontSize}px`,
-  xl: `${baseFontSize * 1.25}px`,
-  "2xl": `${baseFontSize * 1.563}px`,
-  "3xl": `${baseFontSize * 1.953}px`,
-  "4xl": `${baseFontSize * 2.441}px`,
-  "5xl": `${baseFontSize * 3.052}px`,
+export const images = {
+  welcome: welcome,
 };
