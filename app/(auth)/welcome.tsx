3564072097a44c86ecb7de79e3e1e48d7afa13ac
Chethaka Sathmal <csathmal24@gmail.com>
2024-11-25 09:14:48 +0530

    finished welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, View } from "react-native";
+import CustomButton from "@/components/customButton";
+
+import { images } from "@/constants";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
-    <Text className="font-bold bg-red-200 font-thin-italic pl-10 text-5xl">
-      Welcome
-    </Text>
+    <SafeAreaView className="flex items-center bg-main h-full pt-[10vh]">
+      <Image
+        source={images.welcome}
+        resizeMode="contain"
+        className="h-[40vh]"
+      />
+      <Image
+        source={images.logoWhite}
+        resizeMode="contain"
+        className="w-[80vw]"
+      />
+      <View className="w-[80vw]">
+        <CustomButton
+          title="Create an Account"
+          bgVariant="secondary"
+          textVariant="theme"
+          className="mb-[3vh]"
+          onPress={() => router.push("/(auth)/signUp")}
+        />
+        <CustomButton
+          title="Sign In"
+          bgVariant="outline"
+          textVariant="default"
+          onPress={() => router.push("/(auth)/signIn")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
